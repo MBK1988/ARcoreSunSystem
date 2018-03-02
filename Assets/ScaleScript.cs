feb7fc2a679b6f9130ba6e3c8b08fe0f6da0ea98
Mads Brønd Kracht <mbk@insero.com>
2018-03-02 10:47:35 +0100

    Initial Commit

---
+++ Assets/ScaleScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleScript : MonoBehaviour
+{
+
+    private readonly Vector3 _ScaleSpeed = new Vector3(0.0001f, 0.0001f, 0.0001f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector3 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector3 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            var prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            var touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            var deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // ... change the canvas size based on the change in distance between the touches.
+            transform.localScale -= deltaMagnitudeDiff * _ScaleSpeed;
+
+            if (transform.localScale.y < 0.001f)
+            {
+                transform.localScale = new Vector3(0.001f,0.001f,0.001f);
+            }
+
+            if (transform.localScale.y > 3.00f)
+            {
+                transform.localScale = new Vector3(3f, 3f, 3f);
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Assets/SolarSystem/Script/OrbitScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitScript : MonoBehaviour
+{
+
+
+    public Rigidbody rb;
+    public GameObject sun;
+    public Transform centerSun;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //sun = GameObject.FindGameObjectWithTag("Sun");
+        centerSun = sun.transform;
+        transform.position = (transform.position - centerSun.position).normalized * radius + centerSun.position;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        transform.RotateAround(centerSun.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - centerSun.position).normalized * radius + centerSun.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+
+
+    }
+}
